a62e758808c486cadae782fb0702426b4966b1db
Nico Lora <143099690+NicoLora@users.noreply.github.com>
2023-09-05 09:55:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,15 @@ radio.on_received_number(on_recieved_number)
 
 def on_button_pressed_a():
     radio.send_number(0)
-    
+    sphero.set_raw_motors(sphero.RawMotorModes.FORWARD, 50, sphero.RawMotorModes.Forward, 50)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    radio.send_number(1)
+    sphero.set_raw_motors(sphero.RawMotorModes.BACKWARD, 50, sphero.RawMotorModes.ON, 50)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_button_pressed_ab():
+    radio.send_number(2)
+    sphero.set_raw_motors(sphero.RawMotorModes.OFF, 0, sphero.RawMotorModes.OFF, 0)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
