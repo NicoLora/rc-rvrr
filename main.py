c3dd0e4ead23a876b1978860981a8b2cf1b1384d
Nico Lora <143099690+NicoLora@users.noreply.github.com>
2023-09-05 10:52:29 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,26 +1,55 @@
+#import time
 radio.set_group(10)
 
 def on_recieved_number(receivedNumber):
     if receivedNumber == 0:
-        sphero.set_raw_motors(sphero.RawMotorModes.FORWARD, 50, sphero.RawMotorModes.Forward, 50)
+        basic.show_icon(IconNames.HAPPY)
+        sphero.set_raw_motors(sphero.RawMotorModes.FORWARD, 100, sphero.RawMotorModes.FORWARD, 100)
     if receivedNumber == 1:
-        sphero.set_raw_motors(sphero.RawMotorModes.BACKWARD, 50, sphero.RawMotorModes.ON, 50)
+        basic.show_icon(IconNames.SAD)
+        sphero.set_raw_motors(sphero.RawMotorModes.BACKWARD, 100, sphero.RawMotorModes.BACKWARD, 100)
     if receivedNumber == 2:
+        basic.show_icon(IconNames.ASLEEP)
         sphero.set_raw_motors(sphero.RawMotorModes.OFF, 0, sphero.RawMotorModes.OFF, 0)
 
 radio.on_received_number(on_recieved_number)
 
 def on_button_pressed_a():
     radio.send_number(0)
-    sphero.set_raw_motors(sphero.RawMotorModes.FORWARD, 50, sphero.RawMotorModes.Forward, 50)
+#    sphero.set_raw_motors(sphero.RawMotorModes.FORWARD, 100, sphero.RawMotorModes.FORWARD, 100)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     radio.send_number(1)
-    sphero.set_raw_motors(sphero.RawMotorModes.BACKWARD, 50, sphero.RawMotorModes.ON, 50)
+#    sphero.set_raw_motors(sphero.RawMotorModes.BACKWARD, 100, sphero.RawMotorModes.BACKWARD, 100)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_button_pressed_ab():
     radio.send_number(2)
-    sphero.set_raw_motors(sphero.RawMotorModes.OFF, 0, sphero.RawMotorModes.OFF, 0)
+#    sphero.set_raw_motors(sphero.RawMotorModes.OFF, 0, sphero.RawMotorModes.OFF, 0)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+
+def on_forever():
+    if input.light_level()==0:
+        sphero.set_rgb_led_by_index(sphero.LEDs.RIGHT_HEADLIGHT, 255, 255, 255)
+        sphero.set_rgb_led_by_index(sphero.LEDs.LEFT_HEADLIGHT, 255, 255, 255)
+        sphero.set_rgb_led_by_index(sphero.LEDs.RIGHT_BRAKELIGHT, 255, 0, 0)
+        sphero.set_rgb_led_by_index(sphero.LEDs.LEFT_BRAKELIGHT, 255, 0, 0)
+    else:
+        sphero.set_all_leds(0,0,0)                        
+basic.forever(on_forever)
+
+
+# while True:
+#     #display.scroll(display.read_light_level())
+#     #sleep(1000)
+#     if display.read_light_level()==0:
+#         RVRLed.set_rgb_led_by_index(LEDs.RIGHT_BRAKELIGHT, red=255, green=0, blue=0)
+#         RVRLed.set_rgb_led_by_index(LEDs.LEFT_BRAKELIGHT, red=255, green=0, blue=0)
+#         RVRLed.set_rgb_led_by_index(LEDs.RIGHT_HEADLIGHT, red=255, green=255, blue=255)
+#         RVRLed.set_rgb_led_by_index(LEDs.LEFT_HEADLIGHT, red=255, green=255, blue=255)
+
+#     else:
+#         RVRLed.set_all_leds(0,0,0)
+        
--- main.ts
+++ main.ts
@@ -1,5 +1,42 @@
+// import time
 radio.setGroup(10)
-function on_button_pressed_a() {
+radio.onReceivedNumber(function on_recieved_number(receivedNumber: number) {
+    if (receivedNumber == 0) {
+        basic.showIcon(IconNames.Happy)
+        sphero.setRawMotors(sphero.RawMotorModes.forward, 100, sphero.RawMotorModes.forward, 100)
+    }
+    
+    if (receivedNumber == 1) {
+        basic.showIcon(IconNames.Sad)
+        sphero.setRawMotors(sphero.RawMotorModes.backward, 100, sphero.RawMotorModes.backward, 100)
+    }
+    
+    if (receivedNumber == 2) {
+        basic.showIcon(IconNames.Asleep)
+        sphero.setRawMotors(sphero.RawMotorModes.off, 0, sphero.RawMotorModes.off, 0)
+    }
+    
+})
+//     sphero.set_raw_motors(sphero.RawMotorModes.FORWARD, 100, sphero.RawMotorModes.FORWARD, 100)
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     radio.sendNumber(0)
-}
-
+})
+//     sphero.set_raw_motors(sphero.RawMotorModes.BACKWARD, 100, sphero.RawMotorModes.BACKWARD, 100)
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    radio.sendNumber(1)
+})
+//     sphero.set_raw_motors(sphero.RawMotorModes.OFF, 0, sphero.RawMotorModes.OFF, 0)
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    radio.sendNumber(2)
+})
+basic.forever(function on_forever() {
+    if (input.lightLevel() == 0) {
+        sphero.setRgbLedByIndex(sphero.LEDs.rightHeadlight, 255, 255, 255)
+        sphero.setRgbLedByIndex(sphero.LEDs.leftHeadlight, 255, 255, 255)
+        sphero.setRgbLedByIndex(sphero.LEDs.rightBrakelight, 255, 0, 0)
+        sphero.setRgbLedByIndex(sphero.LEDs.leftBrakelight, 255, 0, 0)
+    } else {
+        sphero.setAllLeds(0, 0, 0)
+    }
+    
+})
